413e5175a22960ae63eaa287462501a02bdfd886
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2024-11-21 07:57:58 +0600

    Practice1.c

---
+++ Practice1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++) //Rows
+    {
+        for(j=1;j<=i;j++) //Columns
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
